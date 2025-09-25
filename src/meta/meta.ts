73ff1a4de0842d0bb46e8526c0ca9e42b34665af
Norah <sq11y@proton.me>
2025-09-25 12:56:07 +0200

    Reformatting

--- src/meta/meta.ts
+++ src/meta/meta.ts
@@ -2,6 +2,8 @@ import { readFileSync } from "node:fs";
 
 import { parse } from "@vue/compiler-sfc";
 
+import { PropertyMeta } from "vue-component-meta";
+
 import type { Tag, Checker, ComponentMeta } from "./types.js";
 
 export const generateComponentMeta = (
@@ -11,101 +13,65 @@ export const generateComponentMeta = (
 ): ComponentMeta => {
   const meta = checker.getComponentMeta(url);
 
-  const content = readFileSync(url, "utf-8");
-
-  /* prettier-ignore */
-  const {
-    descriptor: { customBlocks },
-  } = parse(content, {
-    sourceMap: false,
-    ignoreEmpty: true,
-  });
-
-  const docs = customBlocks.find((i) => {
-    return i.type === "docs" && i.lang === "md";
-  });
-
-  const maybeRender = (markdown?: string) => {
-    return markdown ? render(markdown) : markdown;
-  };
+  const _render = (markdown?: string) => (markdown ? render(markdown) : markdown);
 
   const nonGlobalProps = meta.props.filter((prop) => !prop.global);
 
-  /* prettier-ignore */
-  const { modelValues: _modelValues = [], props: _props = [] } = Object.groupBy(nonGlobalProps, (p) => {
-    return meta.events.some((event) => event.name === `update:${p.name}`)
-      ? "modelValues"
-      : "props";
+  const { modelValues = [], props = [] } = Object.groupBy(nonGlobalProps, (p) => {
+    return meta.events.some((event) => event.name === `update:${p.name}`) ? "modelValues" : "props";
   });
 
-  const modelValues = _modelValues.map((mv) => ({
-    name: mv.name,
-    type: mv.type,
-    tags: filterTags(mv.tags),
-    required: mv.required,
-    default: getDefault(mv),
-    description: maybeRender(getDescription(mv)),
-    deprecated: getDeprecated(mv.tags),
-  }));
-
-  const props = _props.map((p) => ({
+  const commonMapper = (p: Omit<PropertyMeta, "required" | "global" | "schema">) => ({
     name: p.name,
     type: p.type,
-    tags: filterTags(p.tags),
-    required: p.required,
-    default: getDefault(p),
-    description: maybeRender(getDescription(p)),
+    tags: p.tags.filter((t) => !["default", "deprecated"].includes(t.name)),
+    description: _render(getDescription(p)),
     deprecated: getDeprecated(p.tags),
-  }));
+  });
 
-  const events = meta.events
-    .filter((e) => !modelValues.find((prop) => `update:${prop.name}` === e.name))
-    .map((e) => ({
-      name: e.name,
-      type: e.type,
-      tags: filterTags(e.tags),
-      description: maybeRender(getDescription(e)),
-      deprecated: getDeprecated(e.tags),
-    }));
+  const propMapper = (p: PropertyMeta) => ({
+    ...commonMapper(p),
 
-  const slots = meta.slots.map((s) => ({
-    name: s.name,
-    type: s.type,
-    description: maybeRender(getDescription(s)),
-  }));
+    required: p.required,
+    default: p.default ?? getTagText(p.tags, "default"),
+  });
+
+  const content = readFileSync(url, "utf-8");
+
+  const { descriptor } = parse(content, { sourceMap: false, ignoreEmpty: true });
+
+  const docs = descriptor.customBlocks.find((i) => i.type === "docs" && i.lang === "md");
 
   return {
-    modelValues,
-    description: maybeRender(docs?.content),
-    props,
-    events,
-    slots,
+    description: _render(docs?.content),
+
+    modelValues: modelValues.map((mv) => propMapper(mv)),
+    props: props.map((p) => propMapper(p)),
+
+    events: meta.events
+      .filter((e) => !modelValues.find((mv) => `update:${mv.name}` === e.name))
+      .map((e) => commonMapper(e)),
+
+    slots: meta.slots.map((s) => ({
+      name: s.name,
+      type: s.type,
+      description: _render(getDescription(s)),
+    })),
   };
 };
 
-const filterTags = (tags: Tag[]) => {
-  return tags.filter((t) => !["default", "deprecated"].includes(t.name));
-};
-
 const getTagText = (tags: Tag[], tag: string) => {
-  return tags.find(({ name }) => name === tag)?.text;
+  /* eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing */
+  return tags.find(({ name }) => name === tag)?.text || undefined;
 };
 
 const getDescription = (meta: { tags?: Tag[]; description?: string }) => {
-  return meta.description ?? getTagText(meta.tags ?? [], "Description") ?? undefined;
-};
-
-const getDefault = (meta: { tags: Tag[]; default?: string }) => {
-  return meta.default ?? getTagText(meta.tags, "default") ?? undefined;
+  return meta.description ?? getTagText(meta.tags ?? [], "Description");
 };
 
 const getDeprecated = (tags: Tag[]): undefined | string | true => {
   const tag = tags.find(({ name }) => name === "deprecated");
 
-  if (!tag) {
-    return undefined;
-  }
-
   /* eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing */
-  return tag.text || true;
+  return tag ? tag.text || true : undefined;
 };

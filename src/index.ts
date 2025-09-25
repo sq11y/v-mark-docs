8bf9298ff2a8addb1729ec5085d70532167c7c0f
Norah <sq11y@proton.me>
2025-09-25 14:46:04 +0200

    Use v-custom-block

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,11 @@
-import docs from "./docs.js";
 import markdown, { MarkdownPluginOptions } from "./markdown.js";
 
 import type { PluginOption } from "vite";
+import customBlock from "v-custom-block";
 
+export type { MarkdownPluginOptions } from "./markdown.js";
 export type { ComponentMeta } from "./meta/types.js";
 
-export type PluginOptions = MarkdownPluginOptions & {
-  /**
-   * If the docs for a component should
-   * be made available on it under `Component.__docs`.
-   */
-  exposeDocs?: boolean;
-};
-
-export default (options: PluginOptions): PluginOption[] => {
-  return [markdown(options), docs(options.exposeDocs)];
+export default (options: MarkdownPluginOptions): PluginOption[] => {
+  return [markdown(options), customBlock("docs")];
 };
